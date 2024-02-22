e1fd18208fc7f6f16474e28c7753c7f9bed04a03
youngmaitas <seyoit@gmail.com>
2024-02-22 09:10:25 +0900

    PF

---
+++ imexer/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './coms/Nav'
+
+function App() {
+  return (
+    <Nav/>
+  );
+}
+
+export default App;
