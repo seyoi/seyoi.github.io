1414a4b848d0ee1c4b11cd932021ba3a0d8de6ee
youngmatias <seyoit@gmail.com>
2024-02-28 19:02:51 +0900

    minipj

--- react/miniprojects/src/pages/Home.js
+++ react/miniprojects/src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
   
 
   return ( <>
-   
+   click projects
 </>  );
 }
 
--- react/miniprojects/src/pages/projects/p9/P9.js
+++ react/miniprojects/src/pages/projects/p9/P9.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './p9.css'
 const P9 = () => {
+  
   return (
     <div className="p9">
       <div className="video">
@@ -8,7 +9,8 @@ const P9 = () => {
           <iframe
             width="100%"
             height="100vh"
-            src="https://www.youtube.com/embed/JvFK-bvZxTQ"  // YouTube 동영상의 임베드 링크를 사용합니다.
+            src="https://www.youtube.com/embed/JvFK-bvZxTQ?autoplay=1&controls=1"
+
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
