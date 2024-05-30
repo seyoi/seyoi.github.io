3c6df00414de20ec745f21de56a2426e3cc3f4dd
youngmatias <seyoit@gmail.com>
2024-05-30 12:19:01 +0900

    30May

--- deploy/src/components/common/forms/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-function Checkbox(props: any) {
-  return (
-    <label id={props.id} className="inline-block p-2 align-middle">
-      <input type="checkbox" name={props.name} id={props.id} />
-    </label>
-  );
-}
-
-export default Checkbox;
