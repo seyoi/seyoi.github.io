0fd1313409dd14c5eaeeef44487430e3b2c30033
seyoi <seyoit@gmail.com>
2024-09-21 10:15:38 +0900

    fififi

---
+++ fectumarte/src/app/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (<>
+  <div className='ml-10 mt-20'>
+     <div>Fectumarte</div>
+    <div>Software Service as Super</div><br></br>
+    <div>Business Registerd Company
+    
+    </div> 
+    <div>Registerd No. 123-38-651110</div><br></br>
+    <div>Mobile +8210 4222 6836</div>
+    <div>Email seyoit@gmail.com</div>
+    <div>Fectumarte 2024</div></div>
+   
+  </>)
+}
+
+export default App
+
